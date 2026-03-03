a541cf3ddd69b73465ba97dba9fd4520b4c31de3
Artem Bagno <bgn.rtm@gmail.com>
2026-03-03 19:05:12 +0200

    fix main ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "../src/task-1";
-import "../src/task-2";
-import "../src/task-3";
-import "../src/task-4";
-import "../src/task-5";
-import "../src/task-6";
-import "../src/task-7";
-import "../src/task-8";
-import "./index.css";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+import '../src/task-1';
+import '../src/task-2';
+import '../src/task-3';
+import '../src/task-4';
+import '../src/task-5';
+import '../src/task-6';
+import '../src/task-7';
+import '../src/task-8';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>{/* <App /> */}</StrictMode>,
-);
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>{/* <App /> */}</StrictMode>,
+// );
